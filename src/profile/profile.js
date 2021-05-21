b41d31bdb7a792d5ed852b80ce2a3f480d0a915e
chand10-beg <aitshu.844@gmail.com>
2021-05-21 19:18:11 +0530

    updating edit field

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -4,6 +4,7 @@ import Table from '@material-ui/core/Table';
 import {useState, useEffect} from 'react';
 import { FiEdit} from 'react-icons/fi';
 import axios from 'axios';
+import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -11,13 +12,28 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
 });
-
 
 
 const Profile =({history, match}) => {
@@ -71,7 +87,17 @@ const Profile =({history, match}) => {
         //console.log(props);
       };
       const classes = useStyles();
-
+      const [modalStyle] = React.useState(getModalStyle);
+      const [open, setOpen] = React.useState(false);
+    
+      const handleOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
+    
   return (
       <div style={{alignContent: 'center'}}>
       <Paper style={{padding: '5rem'}}>
@@ -100,7 +126,68 @@ const Profile =({history, match}) => {
               <TableCell align="right">{username}</TableCell>
               <TableCell align="right">{email}</TableCell>
               <TableCell align="right">{address}</TableCell>
-              <TableCell align="right"><FiEdit/></TableCell>
+         
+              <TableCell align="right"><Button   onClick={handleOpen} ><FiEdit/></Button>    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <Paper style={{padding: '1rem'}}><h1>Updating values</h1><Grid container direction= "column" justify="center">
+        <Grid item xs={12}>  <TextField
+        
+            type="text"
+            // value={email}
+            // onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            label="Email"
+            // tabIndex={1}
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          /><Button style={{size: '0.5rem', marginLeft: '1rem', marginTop: '1rem'}} variant="contained" color="primary">
+        Update 
+        </Button>
+        
+          </Grid>
+          <Grid item xs={12}> 
+           <TextField
+         
+
+            type="text"
+            // value={email}
+            // onChange={(e) => setEmail(e.target.value)}
+            name="username"
+            label="Username"
+            // tabIndex={1}
+            className="form-control"
+            id="username"
+            aria-describedby="username"
+          /><Button style={{size: '0.5rem', marginLeft: '1rem', marginTop: '1rem'}} variant="contained" color="primary">
+        Update 
+        </Button>
+        </Grid>
+          <Grid item xs={12}> 
+           <TextField
+         
+
+            type="text"
+            // value={email}
+            // onChange={(e) => setEmail(e.target.value)}
+            name="address"
+            label="address"
+            // tabIndex={1}
+            className="form-control"
+            id="address"
+            aria-describedby="address"
+          /><Button style={{size: '0.5rem', marginLeft: '1rem', marginTop: '1rem'}} variant="contained" color="primary">
+          Update
+        </Button>
+        
+          </Grid>
+          </Grid> </Paper>
+        
+      </Modal> </TableCell>
               {/* <TableCell align="right">{row.protein}</TableCell> */}
             </TableRow>
           
